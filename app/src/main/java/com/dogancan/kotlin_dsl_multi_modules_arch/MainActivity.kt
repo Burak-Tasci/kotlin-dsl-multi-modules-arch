ede5e7681f5aabca5969d97b04b3d91504ca60c0
buraaktascii <buraaktascii@outlook.com>
2022-09-09 14:17:52 +0300

    update | navigation structure

--- app/src/main/java/com/dogancan/kotlin_dsl_multi_modules_arch/MainActivity.kt
+++ app/src/main/java/com/dogancan/kotlin_dsl_multi_modules_arch/MainActivity.kt
@@ -4,61 +4,42 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
-import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.dogancan.kotlin_dsl_multi_modules_arch.extensions.setupWithNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     private val viewModel: MainViewModel by viewModels()
-    private var currentNavController: LiveData<NavController>? = null
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null)
-            initBottomNavigationView()
-    }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+        navController = navHostFragment.navController
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        initBottomNavigationView()
-    }
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigationView.setupWithNavController(navController)
 
-    private fun initBottomNavigationView() {
-        val bottomNavigationView =
-            findViewById<BottomNavigationView>(R.id.bottom_nav)
-
-        val navGraphs = listOf(
-            R.navigation.character,
-            R.navigation.episode
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.character, R.id.episode)
         )
 
-        val controller = bottomNavigationView.setupWithNavController(
-            navGraphIds = navGraphs,
-            fragmentManager = supportFragmentManager,
-            containerId = R.id.nav_host_container,
-            intent = intent
-        )
-
-        controller.observe(this) { navController ->
-            setupActionBarWithNavController(navController)
-        }
-
-        currentNavController = controller
-
-        currentNavController?.observe(this) { navController ->
-            navController.addOnDestinationChangedListener { _, destination, args ->
-                bottomNavigationView.isVisible = destination.id != R.id.characterDetailFragment
-            }
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            bottomNavigationView.isVisible = destination.id != R.id.characterDetailFragment
         }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return currentNavController?.value?.navigateUp() ?: false
+        return navController.navigateUp(appBarConfiguration)
     }
 }
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,122 +3,122 @@
  */
 
 
-    object Releases {
-        val versionCode = 1
-        val versionName = "1.0"
-    }
+object Releases {
+    val versionCode = 1
+    val versionName = "1.0"
+}
 
-    object Versions {
-        val kotlin = "1.3.21"
-        val gradle = "3.3.2"
-        val compileSdka = 28
-        var minSdk = 23
-        val targetSdk = 28
-        val appCompat = "1.1.0-alpha04"
-        val coreKtx = "1.1.0-alpha04"
-        val constraintLayout = "1.1.3"
-        val junit = "4.12"
-        val androidTestRunner = "1.1.2-alpha02"
-        val espressoCore = "3.2.0-alpha02"
-        val retrofit = "2.9.0"
-        val retrofitCoroutines = "0.9.2"
-        val retrofitGson = "2.9.0"
-        val gson = "2.8.8"
-        val coroutines = "1.6.0"
-        val koin = "1.0.2"
-        val timber = "4.7.1"
-        val lifecycle = "2.4.0"
-        val nav = "2.3.5"
-        val room = "2.1.0-alpha06"
-        val recyclerview = "1.0.0"
-        val safeArgs = "2.4.2"
-        val glide = "4.9.0"
-        val mockwebserver = "2.7.5"
-        val archCoreTest = "2.0.0"
-        val androidJunit = "1.1.0"
-        val mockk = "1.9.2"
-        val fragmentTest = "1.1.0-alpha06"
-        val databinding = "3.3.2"
-        val hilt = "2.41"
-        val okHttp = "4.7.2"
-        val sandwich = "1.2.4"
-        val activityExt = "1.2.3"
-    }
+object Versions {
+    val kotlin = "1.3.21"
+    val gradle = "3.3.2"
+    val compileSdka = 28
+    var minSdk = 23
+    val targetSdk = 28
+    val appCompat = "1.1.0-alpha04"
+    val coreKtx = "1.1.0-alpha04"
+    val constraintLayout = "1.1.3"
+    val junit = "4.12"
+    val androidTestRunner = "1.1.2-alpha02"
+    val espressoCore = "3.2.0-alpha02"
+    val retrofit = "2.9.0"
+    val retrofitCoroutines = "0.9.2"
+    val retrofitGson = "2.9.0"
+    val gson = "2.8.8"
+    val coroutines = "1.6.0"
+    val koin = "1.0.2"
+    val timber = "4.7.1"
+    val lifecycle = "2.4.0"
+    val nav = "2.5.2"
+    val room = "2.1.0-alpha06"
+    val recyclerview = "1.0.0"
+    val safeArgs = "2.4.2"
+    val glide = "4.9.0"
+    val mockwebserver = "2.7.5"
+    val archCoreTest = "2.0.0"
+    val androidJunit = "1.1.0"
+    val mockk = "1.9.2"
+    val fragmentTest = "1.1.0-alpha06"
+    val databinding = "3.3.2"
+    val hilt = "2.41"
+    val okHttp = "4.7.2"
+    val sandwich = "1.2.4"
+    val activityExt = "1.2.3"
+}
 
-    object Libraries {
-        // ROOM
-        val roomCompiler = "androidx.room:room-compiler:${Versions.room}"
-        val roomRunTime = "androidx.room:room-runtime:${Versions.room}"
-        val roomKtx = "androidx.room:room-ktx:${Versions.room}"
+object Libraries {
+    // ROOM
+    val roomCompiler = "androidx.room:room-compiler:${Versions.room}"
+    val roomRunTime = "androidx.room:room-runtime:${Versions.room}"
+    val roomKtx = "androidx.room:room-ktx:${Versions.room}"
 
-        // RETROFIT
-        val retrofitCoroutineAdapter =
-            "com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:${Versions.retrofitCoroutines}"
-        val gson = "com.google.code.gson:gson:${Versions.gson}"
-        val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
-        val retrofitGsonConverter = "com.squareup.retrofit2:converter-gson:${Versions.retrofitGson}"
-        val httpLoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okHttp}"
+    // RETROFIT
+    val retrofitCoroutineAdapter =
+        "com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:${Versions.retrofitCoroutines}"
+    val gson = "com.google.code.gson:gson:${Versions.gson}"
+    val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    val retrofitGsonConverter = "com.squareup.retrofit2:converter-gson:${Versions.retrofitGson}"
+    val httpLoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okHttp}"
 
-        // GLIDE
-        val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
+    // GLIDE
+    val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
 
-        val daggerHilt = "com.google.dagger:hilt-android:${Versions.hilt}"
-        val daggerHiltCompiler = "com.google.dagger:hilt-compiler:${Versions.hilt}"
-        val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okHttp}"
-        val sandwich ="com.github.skydoves:sandwich:${Versions.sandwich}"
+    val daggerHilt = "com.google.dagger:hilt-android:${Versions.hilt}"
+    val daggerHiltCompiler = "com.google.dagger:hilt-compiler:${Versions.hilt}"
+    val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okHttp}"
+    val sandwich ="com.github.skydoves:sandwich:${Versions.sandwich}"
 
 
-    }
+}
 
-    object KotlinLibraries {
-        val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
-        val kotlinCoroutineCore =
-            "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
-    }
+object KotlinLibraries {
+    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    val kotlinCoroutineCore =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
+}
 
-    object AndroidLibraries {
-        // KOTLIN
-        val kotlinCoroutineAndroid =
-            "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
+object AndroidLibraries {
+    // KOTLIN
+    val kotlinCoroutineAndroid =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
 
-        // ANDROID
-        val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
-        val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
-        val constraintLayout =
-            "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
-        val lifeCycleViewModel = "androidx.lifecycle:lifecycle-viewmodel:${Versions.lifecycle}"
-        val lifecycleViewModelExt = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
-        val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}"
-        val lifecycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycle}"
-        val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerview}"
-        val navigation = "androidx.navigation:navigation-ui-ktx:${Versions.nav}"
-        val navigationFrag = "androidx.navigation:navigation-fragment-ktx:${Versions.nav}"
-        val activityExt ="androidx.activity:activity-ktx:${Versions.activityExt}"
+    // ANDROID
+    val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+    val constraintLayout =
+        "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    val lifeCycleViewModel = "androidx.lifecycle:lifecycle-viewmodel:${Versions.lifecycle}"
+    val lifecycleViewModelExt = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
+    val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}"
+    val lifecycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycle}"
+    val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerview}"
+    val navigation = "androidx.navigation:navigation-ui-ktx:${Versions.nav}"
+    val navigationFrag = "androidx.navigation:navigation-fragment-ktx:${Versions.nav}"
+    val activityExt ="androidx.activity:activity-ktx:${Versions.activityExt}"
 
-    }
+}
 
-    object TestLibraries {
-        // ANDROID TEST
-        val androidTestRunner = "androidx.test:runner:${Versions.androidTestRunner}"
-        val espresso = "androidx.test.espresso:espresso-core:${Versions.espressoCore}"
-        val espressoContrib = "androidx.test.espresso:espresso-contrib:${Versions.espressoCore}"
-        val archCoreTest = "androidx.arch.core:core-testing:${Versions.archCoreTest}"
-        val junit = "androidx.test.ext:junit:${Versions.androidJunit}"
-        val fragmentNav = "androidx.fragment:fragment-testing:${Versions.fragmentTest}"
+object TestLibraries {
+    // ANDROID TEST
+    val androidTestRunner = "androidx.test:runner:${Versions.androidTestRunner}"
+    val espresso = "androidx.test.espresso:espresso-core:${Versions.espressoCore}"
+    val espressoContrib = "androidx.test.espresso:espresso-contrib:${Versions.espressoCore}"
+    val archCoreTest = "androidx.arch.core:core-testing:${Versions.archCoreTest}"
+    val junit = "androidx.test.ext:junit:${Versions.androidJunit}"
+    val fragmentNav = "androidx.fragment:fragment-testing:${Versions.fragmentTest}"
 
-        // KOIN
-        val koin = "org.koin:koin-test:${Versions.koin}"
+    // KOIN
+    val koin = "org.koin:koin-test:${Versions.koin}"
 
-        // MOCK WEBSERVER
-        val mockWebServer = "com.squareup.okhttp:mockwebserver:${Versions.mockwebserver}"
+    // MOCK WEBSERVER
+    val mockWebServer = "com.squareup.okhttp:mockwebserver:${Versions.mockwebserver}"
 
-        // MOCK
-        val mockkAndroid = "io.mockk:mockk-android:${Versions.mockk}"
-        val mockk = "io.mockk:mockk:${Versions.mockk}"
+    // MOCK
+    val mockkAndroid = "io.mockk:mockk-android:${Versions.mockk}"
+    val mockk = "io.mockk:mockk:${Versions.mockk}"
 
-        // COROUTINE
-        val coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutines}"
+    // COROUTINE
+    val coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutines}"
 
-        // DATA BINDING
-        val databinding = "androidx.databinding:databinding-compiler:${Versions.databinding}"
-    }
+    // DATA BINDING
+    val databinding = "androidx.databinding:databinding-compiler:${Versions.databinding}"
+}
